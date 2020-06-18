b7279d1af78c8e31f61d65d51000e0f723b7b537
Lane Le Prevost-Smith <lanelps@gmail.com>
2020-06-18 17:58:22 +1200

    Public changed to build

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,28 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/client/index.js',
-  output: {
-    path: path.join(__dirname, './public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/client/index.html'
-    })
-  ]
+	entry: './src/client/index.js',
+	output: {
+		path: path.join(__dirname, './build'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/client/index.html'
+		})
+	]
 }
